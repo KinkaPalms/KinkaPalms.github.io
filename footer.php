e6a9920aa111117b689d6954e297c34c3177ea59
MooseValley <Moose_Software@yahoo.com.au>
2021-05-15 17:03:08 +1000

    15-May-2021
    15-May-2021: Farewell Twin Apartment S3, Increase our Peak Rates

---
+++ footer.php
@@ -0,0 +1,32 @@
+DOES NOT work on Tripod.com - PHP not available ???
+
+<?php
+include_once ('footer.php');
+?>
+
+
+
+<!-- footer.php -->
+<p><br>
+<a href="index.html">Home</a>
+
+<hr>
+You are visitor number:
+<!-- Start of StatCounter Code for Tripod -->
+<script type="text/javascript">
+var sc_project=9217780;	var sc_invisible=0;	var sc_security="5fe485b5";
+var scJsHost = (("https:" == document.location.protocol) ?	"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +	scJsHost+	"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="hits counter"	href="http://statcounter.com/" target="_blank"><img class="statcounter" src="http://c.statcounter.com/9217780/0/5fe485b5/0/" alt="hits counter"></a></div></noscript>
+<!-- End of StatCounter Code for Tripod -->
+since 5-Sep-2013.
+<a href="http://statcounter.com/p9217780/?guest=1">View My Stats</a>
+<br>
+
+This page is part of <a href="index.html">Kinka Palms Beachfront Apartments / Motel</a> official web site.
+This page was last updated on
+<script type="text/javascript">var x = new Date(document.lastModified); document.write(x);</script>.
+<hr>
+</BODY>
+</HTML>
---
+++ header_and_menus.php
@@ -0,0 +1,89 @@
+DOES NOT work on Tripod.com - PHP not available ???
+
+<?php
+include_once ('header_and_menus.php');
+?>
+
+
+
+<!-- header_and_menus.php -->
+<!DOCTYPE html>
+<html lang="en">
+<!-- %%NOBANNER%% -->
+<!--                 Lovely Hand-Coded HTML by Moose O'Malley - http://moosevalley.fhost.com.au/  -->
+<head>
+	<title>Kinka Palms Beachfront Apartments / Motel - Perfect for your next holiday or break ! ! . . . . . . . . . . . . . </title>
+	<meta name="Author" content="Moose">
+	<meta name="KeyWords" content="Capricorn Coast, Central Queensland, Kinka Beach, Yeppoon, Emu Park, self contained, kitchen, free wireless broadband, dvd player, pool,pet friendly, dog friendly, cat friendly, air conditioned, air-conditioned, ">
+	<META name="description" content="">
+	<link rel="stylesheet" type="text/css" href="pro_drop_1/pro_drop_1.css" />
+	<!--<link rel="stylesheet" type="text/css" href="style_sheet.css" />-->
+	<script src="pro_drop_1/stuHover.js" type="text/javascript"></script>
+</head>
+
+<body text="#000000" bgcolor="#ffffff" link="#0000FF" vlink="#800080" alink="#009900"> <!-- background="pics/goldsand.gif">-->
+
+<IMG SRC="pics/kinka_palms_page_banner.jpg" WIDTH=100%>
+
+
+<span class="preload1"></span>
+<span class="preload2"></span>
+
+<ul id="nav">
+	<li class="top"><a href="index.html" class="top_link"><span>Home</span></a></li>
+	<li class="top"><a href="accommodation__facilities.html" id="Accommodation" class="top_link"><span class="down">Accommodation</span></a>
+		<ul class="sub">
+			<li><a href="accommodation__facilities.html">Facilities</a></li>
+			<li><a href="accommodation__apartment_room_configurations.html">Apartment / Room Configurations</a></li>
+			<li><a href="accommodation__room_rates_pricing.html">Room Rates / Pricing</a></li>
+			<li><a href="accommodation__location__how_to_find_us.html">Location / How to Find Us</a></li>
+			<li><a href="accommodation__cancellation_policy.html">Cancellation Policy</a></li>
+			<li><a href="accommodation__room_rates_pricing.html">Room Rates / Pricing</a></li>
+			<li><a href="accommodation__terms_and_conditions.html">Terms and Conditions</a></li>
+			<li><a href="accommodation__brochure_flyer.html">Brochure / Flyer</a></li>
+		</ul>
+	</li>
+	<li class="top"><a href="media__photos.html" id="photos" class="top_link"><span class="down">Picture Tour</span></a>
+		<ul class="sub">
+			<li><a href="media__photos__Kinka_Beach.html">Photos - Kinka Beach</a></li>
+			<li><a href="media__photos__Kinka_Palms_Motel_Outside.html">Photos - Kinka Palms Motel - Outside </a></li>
+			<li><a href="media__photos__Kinka_Palms_Motel_Inside_Double_Apartment.html">Photos - Double Apartment S2 (sleeps 1-2 people)</a></li>
+			<li><a href="media__photos__Kinka_Palms_Motel_Inside_Twin_Apartment_S3.html">Photos - Twin Apartment S3 (sleeps 1-3 people)</a></li>
+			<li><a href="media__photos__Kinka_Palms_Motel_Inside_Family_Apartment_S4.html">Photos - Family Apartment S4 (sleeps 1-4 people)</a></li>
+	 		<li><a href="media__photos__Kinka_Palms_Motel_Inside_Family_Apartment_S5.html">Photos - Family Apartment S5 (sleeps 1-5 people)</a></li>
+			<li><a href="media__photos__Kinka_Palms_Motel_Inside_Double_Pet_Friendly.html">Photos - "Pet Friendly" Double Apartment S2 (sleeps 1-2 people, 1-2 pets)</a></li>
+			<li><a href="media__photos__Kinka_Palms_Motel_Apartment_Plans.html">Apartment Plans / Designs</a></li>
+			<li><a href="media__videos.html">Videos</a></li>
+		</ul>
+	</li>
+	<li class="top"><a href="region__meals_restaurants_take_aways_supermarkets.html" id="region" class="top_link"><span class="down">Region</span></a>
+		<ul class="sub">
+			<li><a href="region__meals_restaurants_take_aways_supermarkets.html">Meals, Restaurants, Take Aways, Supermarkets, etc</a></li>
+			<li><a href="region__weather_tide_times_fishing_zones.html">Weather Forecasts, Tide Times, Fishing Zones</a></li>
+			<li><a href="region__great_climate_and_virtually_no_jellyfish.html">Great Climate, Calm Ocean, and (virtually) No Jellyfish !!</a></li>
+			<li><a href="region__local_attractions.html">Local Attractions</a></li>
+			<li><a href="region__public_transport.html">Public Transport</a></li>
+			<li><a href="region__car_hire.html">Car Hire</a></li>
+		</ul>
+	</li>
+	<li class="top"><a href="pet_friendly.html" id="pet_friendly" class="top_link"><span class="down">Pet Friendly</span></a>
+		<ul class="sub">
+			<li><a href="pet_friendly.html">Information and Responsibilities</a></li>
+			<li><a href="pet_friendly__what_does_pet_friendly_mean.html">What does "Pet Friendly" mean ?</a></li>
+			<li><a href="pet_friendly__actitivies_and_things_to_do.html">Pets Friendly Activities</a></li>
+			<li><a href="pet_friendly__leads_leashes_dogs_on_beaches.html">Leads/ Leashes and Dogs on Beaches</a></li>
+		</ul>
+	</li>
+	<li class="top"><a href="customer_testimonials.html" id="testimonials" class="top_link"><span class="down">Testimonials</span></a>
+		<ul class="sub">
+			<li><a href="customer_testimonials.html">Customer Testimonials</a></li>
+		</ul>
+	</li>
+	<li class="top"><a href="web_page_updates_news.html" id="updates" class="top_link"><span class="down">Updates/News</span></a>
+		<ul class="sub">
+			<li><a href="web_page_updates_news.html">WEB Page Updates / News</a></li>
+		</ul>
+	</li>
+	<li class="top"><a href="contact_us.html" id="contact" class="top_link"><span>Contact Us</span></a></li>
+</ul>
+
